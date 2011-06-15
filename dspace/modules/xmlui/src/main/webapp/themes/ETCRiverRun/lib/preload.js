bb62e77affe41042a7eb3adaba6d4058fffaa06a
Jen Whitney <jen.j.whitney@gmail.com>
2011-06-15 13:03:35 -0300

    issue #19 work in progress

--- dspace/modules/xmlui/src/main/webapp/themes/ETCRiverRun/lib/preload.js
+++ dspace/modules/xmlui/src/main/webapp/themes/ETCRiverRun/lib/preload.js
@@ -1,79 +1,45 @@
 $(document).ready(function(){
 
-	// Collapse entries in each record.
-        $('.collapseclass').collapser({
-                target: 'next',
-                targetOnly: 'div',
-		changeText: 0,
-                expandClass: 'expArrow',
-                collapseClass: 'collArrow'
-        });
+// Are we on the community-list page?
+if (location.href.toLowerCase().indexOf( ('community-list').toLowerCase() ) > 0) {
+  $('.ds-artifact-item').css('list-style-type','none');
+}
 
-	// OLD COLLAPSER
-	// Entries in 'community-list' page should collapse.
-	// pos = location.href.toLowerCase().indexOf( ('community-list').toLowerCase() );
-	// if (pos != -1){
+// Collapse the list: hide any lists that follow a community heading
+$('span.community-summary-list ~ ul').hide();
 
-	//	$('.bold + ul').css('display','none');
-
-        //	$('.bold').collapser({
-        //        	target: 'siblings',
-	//                targetOnly: 'ul',
-        //	        changeText: 0,
-        //        	expandClass: 'expArrow',
-	//                collapseClass: 'collArrow'
-        //	});
-
-	// }
-
-	// Hide the whole works if we're not on the community-list pages.
-	pos = location.href.toLowerCase().indexOf( ('community-list').toLowerCase() );
-        if (pos == -1){
-		$('.ListMinus').hide();
-		$('.ListPlus').hide();
-	} else {
-		$('.ds-artifact-item').css('list-style-type','none');
-	}
-
-	// Hide the Plus elements
-	$('.ListMinus').hide();
-	$('.ListMinus + span.bold ~ ul').hide();
-	
+/*
 	// Expansion
-	$("p.ListPlus").click(function(){
+	$("p.list-plus").click(function() {
 		$(this).hide();
-		$(this).next("p.ListMinus").show();
-		//slideDown animation doesn't work in IE6:
-		if(navigator.userAgent.match("MSIE 6")) 
-		{
-		    $(this).parent().find("p.ListPlus").hide();
-		    $(this).parent().find("p.ListMinus").show();
-		    $(this).parent().find("p.ListMinus + span.bold ~ ul").show();
-		}
-		else
-		{
+		$(this).next("p.list-minus").show();
+	
+		// slideDown animation doesn't work in IE6:
+		if (navigator.userAgent.match("MSIE 6")) {
+		    $(this).parent().find("p.list-plus").hide();
+		    $(this).parent().find("p.list-minus").show();
+		    $(this).parent().find("p.list-minus + span.bold ~ ul").show();
+		} else {
 		    $(this).parent().children("ul").slideDown("fast");
 		}
-	});
-
+  });
 
 	// Contraction
-	$("p.ListMinus").click(function(){
+	$("p.list-minus").click(function(){
 		$(this).hide();
-		$(this).prev("p.ListPlus").show();
+		$(this).prev("p.list-plus").show();
+		
 		//slideUp animation doesn't work in IE6:
-		if(navigator.userAgent.match("MSIE 6")) 
-		{
-		    $(this).parent().find("p.ListPlus").show();
-		    $(this).parent().find("p.ListMinus").hide();
-		    $(this).parent().find("p.ListMinus + span.bold ~ ul").hide();
-		}
-		else
-		{
+		if (navigator.userAgent.match("MSIE 6")) {
+		    $(this).parent().find("p.list-plus").show();
+		    $(this).parent().find("p.list-minus").hide();
+		    $(this).parent().find("p.list-minus + span.bold ~ ul").hide();
+		} else {
 		    $(this).parent().children("ul").slideUp("fast");
 		}
 	});
-
-
+*/
 });
+
+
 
