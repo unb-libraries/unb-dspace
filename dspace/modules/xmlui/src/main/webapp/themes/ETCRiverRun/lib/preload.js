d4cdfe9328aadc4eceb45f3748ca1a20743df00c
Jen Whitney <jen.j.whitney@gmail.com>
2011-06-16 13:54:20 -0300

    issue #19 - fix expand / collapse display in communities, collections browser
    HTML for expand/collapse arrows dropped in on page load, where needed;
    removed from theme stylesheets. Images replaced with smaller versions,
    floated to left of list items. Expand/collapse images only appear for headings
    followed by lists with multiple sub-items.

--- dspace/modules/xmlui/src/main/webapp/themes/ETCRiverRun/lib/preload.js
+++ dspace/modules/xmlui/src/main/webapp/themes/ETCRiverRun/lib/preload.js
@@ -1,45 +1,66 @@
 $(document).ready(function(){
 
-// Are we on the community-list page?
-if (location.href.toLowerCase().indexOf( ('community-list').toLowerCase() ) > 0) {
-  $('.ds-artifact-item').css('list-style-type','none');
-}
+  // Are we on the community-list page?
+  if (location.href.toLowerCase().indexOf( ('community-list').toLowerCase() ) > 0) {
+    
+    // turn off default list styling
+    $('.ds-artifact-item').css('list-style-type','none');
+    
+    // Collapse the list: hide any lists that follow a community heading
+    $('span.community-summary-list ~ ul').hide();
 
-// Collapse the list: hide any lists that follow a community heading
-$('span.community-summary-list ~ ul').hide();
+    // Add anchors with classes to identify list state: expanded/collapsed, or headings w/o children
+    $('span.community-summary-list').each(function(index) {
+      $(this).before(function() {
+        var listState = 
+          ( $(this).next('ul').children('li').length > 0 ) ?
+          'collapsed' :
+          'no-children';
+        return '<a href="#" class="' + listState + '">&nbsp;</a>';
+      });
+    });
 
-/*
-	// Expansion
-	$("p.list-plus").click(function() {
-		$(this).hide();
-		$(this).next("p.list-minus").show();
-	
-		// slideDown animation doesn't work in IE6:
-		if (navigator.userAgent.match("MSIE 6")) {
-		    $(this).parent().find("p.list-plus").hide();
-		    $(this).parent().find("p.list-minus").show();
-		    $(this).parent().find("p.list-minus + span.bold ~ ul").show();
-		} else {
-		    $(this).parent().children("ul").slideDown("fast");
-		}
-  });
+    // Click to expand:
+     $('.collapsed').live('click', expandList);
 
-	// Contraction
-	$("p.list-minus").click(function(){
-		$(this).hide();
-		$(this).prev("p.list-plus").show();
-		
-		//slideUp animation doesn't work in IE6:
-		if (navigator.userAgent.match("MSIE 6")) {
-		    $(this).parent().find("p.list-plus").show();
-		    $(this).parent().find("p.list-minus").hide();
-		    $(this).parent().find("p.list-minus + span.bold ~ ul").hide();
-		} else {
-		    $(this).parent().children("ul").slideUp("fast");
-		}
-	});
-*/
+    // Click to collapse:
+     $('.expanded').live('click', collapseList);
+  }
+
 });
 
+function expandList(e) {
+  $(this).addClass('expanded');    
+  $(this).removeClass('collapsed');
+
+  // slideDown animation doesn't work in IE6:
+  if (navigator.userAgent.match('MSIE 6')) {
+    $(this).parent().find('span.community-summary-list ~ ul').show();
+  }
+  else {
+    $(this).parent().children('ul').slideDown('fast');
+  }
+ 
+  // return false to discard href attribute value
+  return false;  
+}
+
+function collapseList(e) {
+  $(this).addClass('collapsed');
+  $(this).removeClass('expanded');    
+    
+  // slideDown animation doesn't work in IE6:
+  if (navigator.userAgent.match('MSIE 6')) {
+    $(this).parent().find('span.community-summary-list ~ ul').hide();    
+  } 
+  else {
+    $(this).parent().children('ul').slideUp('fast');
+  }
+  
+  // collapse any expanded children, as well
+  $(this).parent().find('ul li a.expanded').click();
+
+  return false;
+}
 
 
